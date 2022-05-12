cc8e2f8749ad57fbdec3479e2ee027054b4bf178
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-12 12:28:33 +0300

    Initial Commit

---
+++ Alert Kullanimi/Alert_KullanimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alert_KullanimiApp.swift
+//  Alert Kullanimi
+//
+//  Created by Ömer Faruk Kılıçaslan on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Alert_KullanimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
